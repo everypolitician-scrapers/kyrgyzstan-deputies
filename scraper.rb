927a4d420f577af2ee212991a2084d47dc69bcd4
Tony Bowden <tony@tmtm.com>
2017-04-28 10:53:56 +0100

    Remove String#tidy
    Scraped gives us that for free

--- scraper.rb
+++ scraper.rb
@@ -2,20 +2,12 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
+require 'pry'
 require 'scraped'
 require 'scraperwiki'
-require 'nokogiri'
-require 'open-uri'
 
-require 'pry'
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
-
-class String
-  def tidy
-    gsub(/[[:space:]]+/, ' ').strip
-  end
-end
 
 class MembersPage < Scraped::HTML
   decorator Scraped::Response::Decorator::CleanUrls
